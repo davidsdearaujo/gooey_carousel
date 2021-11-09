4dd06593368f3695ed60f2bed88c54d8d2a7c079
Viroj Fernando <fernandoviroj@gmail.com>
2021-11-09 23:01:20 +0530

    turned to nullsafty

--- lib/src/gooey_edge_painter.dart
+++ lib/src/gooey_edge_painter.dart
@@ -3,14 +3,14 @@ import 'gooey_edge.dart';
 
 // This isn't used in the example, but it can be used to quickly paint a GooeyEdge to a canvas.
 class GooeyEdgePainter extends CustomPainter {
-  GooeyEdge edge;
+  late GooeyEdge edge;
 
-  GooeyEdgePainter({@required this.edge});
+  GooeyEdgePainter({required this.edge});
 
   @override
   void paint(Canvas canvas, Size size) {
     Paint fill = new Paint()..color = Colors.blue;
-    canvas.drawPath(edge.buildPath(size), fill);
+    canvas.drawPath(edge.buildPath(size)!, fill);
   }
 
   @override
